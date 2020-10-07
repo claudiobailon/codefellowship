b30ddffccafc5eb0a1494a454f4482b3cc498d75
claudiobailon <c.bailonschubert@gmail.com>
2020-10-06 22:05:39 -0700

    Login not working, added fragments in html

--- src/main/java/com/claudio/codefellowship/controllers/ApplicationUserController.java
+++ src/main/java/com/claudio/codefellowship/controllers/ApplicationUserController.java
@@ -4,11 +4,14 @@ package com.claudio.codefellowship.controllers;
 import com.claudio.codefellowship.models.user.ApplicationUser;
 import com.claudio.codefellowship.models.user.ApplicationUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
+import java.security.Principal;
+import java.sql.Date;
 
 @Controller
 public class ApplicationUserController {
@@ -19,12 +22,18 @@ public class ApplicationUserController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    @GetMapping("/signup")
+    public String showSignup(){
+        return "signup";
+    }
+
+    @DateTimeFormat(pattern = "MM-dd-yyyy")
     @PostMapping("/signup")
     public RedirectView makeNewUser(String username,
                                     String passwordhere,
                                     String firstName,
                                     String lastName,
-                                    String dateOfBirth,
+                                    Date dateOfBirth,
                                     String bio){
 
         passwordhere = passwordEncoder.encode(passwordhere);
@@ -38,8 +47,8 @@ public class ApplicationUserController {
     }
 
     @GetMapping("/login")
-    public String showlogin(){
-        return"login";
+    public String login(){
+        return "login";
     }
 
 
